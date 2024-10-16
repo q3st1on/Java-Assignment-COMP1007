c7e0d5609da294bc01b2e4431701d98821caa981
q3st1on <orlando.morrisjohnson@gmail.com>
2024-10-16 22:45:05 +0800

    blud
    what

---
+++ Submission/Book.java
@@ -0,0 +1,168 @@
+package Assignment;
+import java.io.*;
+import java.util.*;
+
+/*****************************************************
+ * Author : Orlando Morris-Johnson (22222598)        *
+ * Purpose: To store and manage information about    *
+ *          books.                                   * 
+ * Date   : 08/10/2024                               *
+ *****************************************************/
+public class Book
+{
+    private Author[] authors = new Author[3];
+    private int authorCount = 0;
+    private boolean ebook;
+    private String title;
+    private String isbn;
+    private String year;
+    private int edition;
+
+    //Getter functions to expose internal variables from book object
+    public Author[] getAuthors() { return authors; }
+    public boolean isEbook() { return ebook; }
+    public String getTitle() { return title; }
+    public String getISBN() { return isbn; }
+    public String getYear() { return year; }
+    public int getEdition() { return edition; }
+    public int getAuthorCount() { return authorCount; }
+    
+    //Setter functions to expose internal variables from book object
+    public void setEbook(boolean isEbook) { ebook = isEbook; }
+    public void setTitle(String newTitle) { title = newTitle; }
+    public void setISBN(String newISBN) { isbn = newISBN; }
+    public void setEdition(int newEdition) { edition = newEdition; }
+    public void setYear(String newYear) { year = newYear; }
+
+    /*****************************************************
+     * Name   : addAuthor                                *
+     * Date   : 08/10/2024                               *
+     * Import : newAuthor (Author)                       * 
+     * Export : None                                     *
+     * Purpose: To manage addition of authors to a book  *
+     *          and ensure no more than three are added  * 
+     *****************************************************/
+    public void addAuthor(Author newAuthor) {
+        // Check there is space for aditional author
+        if (authorCount < 3)
+        {
+            // Boolean to represent if Author is unique
+            boolean unique = true;
+
+            // Loop through all current authors of book
+            for (int i = 0; i < authorCount; i++)
+            {
+                // Check if new author is the same as existing author
+                if (newAuthor.hashCode() == authors[i].hashCode())
+                {
+                    unique = false;
+                }
+            }
+
+            if (unique)
+            {
+                // If author is unique, add them to the authors array
+                authors[authorCount] = newAuthor;
+
+                // Increment authorCount
+                authorCount ++;
+            }
+            else
+            {
+                // Print error message that the chosen author is already added to the book
+                System.err.println("Error: Author already added to book!");
+            }
+        }
+        else
+        {
+            System.err.println("Error: Cannot add more than 3 Authors to a book");
+        }
+    }
+
+    /*****************************************************
+     * Name   : overwriteAuthor                          *
+     * Date   : 08/10/2024                               *
+     * Import : index (int), newAuthor (Author)          * 
+     * Export : None                                     *
+     * Purpose: To overwrite specific author instance in *
+     *          the books authors array                  * 
+     *****************************************************/
+    public void overwriteAuthor(int index, Author newAuthor)
+    {
+        authors[index] = newAuthor;
+    }
+
+    /*****************************************************
+     * Name   : removeAuthor                             *
+     * Date   : 08/10/2024                               *
+     * Import : index (int)                              * 
+     * Export : None                                     *
+     * Purpose: To remove specific author from authors   *
+     *          array and move other authors if needed   * 
+     *****************************************************/
+    public void removeAuthor(int index)
+    {        
+        int i;
+
+        // Overwrite author at chosen position with null
+        authors[index] = null;
+
+        for (i = index; i < authorCount-1; i++)
+        {
+            // For each author from the chosen position to the final author in the book
+            // overwrite the author with the next author in the array
+            overwriteAuthor(i, authors[i+1]);
+        }
+
+        authors[i] = null;
+        authorCount -= 1;
+    }
+    
+    /*****************************************************
+     * Name   : pruneAuthors                             *
+     * Date   : 08/10/2024                               *
+     * Import : authorIndex (int)                        * 
+     * Export : None                                     *
+     * Purpose: To check if suspected duplicate author   * 
+     *          is in the authors array twice and if so  *
+     *          to remove the duplicate entry            *
+     *****************************************************/
+    public void pruneAuthors(int authorIndex)
+    {
+        boolean duplicate = false;
+
+        // Loop through authors of book
+        for (int i = 0; i < authorCount; i++)
+        {
+            // Ensure we are not checking the suspected entry against itself
+            if (i != authorIndex)
+            {
+                // Check if authors are equal
+                if (authors[i].hashCode() == authors[authorIndex].hashCode())
+                {
+                    // Mark duplicate as true if they are equal
+                    duplicate = true;
+                }
+            }
+        }
+
+        // If the author is a duplicate, remove them from the array
+        if (duplicate)
+        {
+            removeAuthor(authorIndex);
+        }
+    }
+
+    /*****************************************************
+     * Name   : toString                                 *
+     * Date   : 08/10/2024                               *
+     * Import : None                                     * 
+     * Export : None                                     *
+     * Purpose: Creates toString function for testing    * 
+     *****************************************************/
+    @Override
+    public String toString()
+    {
+        return String.format("Title: %-30s | Year: %-6s | Author: %-20s | ISBN: %-14s | Edition: %-3d | Ebook: %b ", title, year, authors[0], isbn, edition, ebook);
+    }
+}
---
+++ Submission/CSVTools.java
@@ -0,0 +1,212 @@
+package Assignment;
+import java.io.*;
+import java.util.*;
+
+
+public class CSVTools {
+    /********************************************************************
+     * Class variables for the CSVTools class:                          *
+     * filePath (String)    : Location of the library data file         *
+     * csvHeader (String[]) : Known good headers for the data file      *
+     ********************************************************************/
+
+    private static String filePath = "StartingDataFile.csv";
+    private static String[] csvHeader = {
+        "title",
+        "familyNameOne",  "firstNameOne",  "nationalityOne",  "birthYearOne",
+        "familyNameTwo",  "firstNameTwo",  "nationalityTwo",  "birthYearTwo",
+        "familyNameThree","firstNameThree","nationalityThree","birthYearThree",
+        "year",
+        "isbn",
+        "ebook",
+        "edition"
+    };
+
+    //Setter Func
+    public static void setFilePath(String newFilePath) { filePath = newFilePath; }
+    
+
+    /*****************************************************
+     * Name   : verifyHeader                             *
+     * Date   : 08/10/2024                               *
+     * Import : testHeader (String[])                    * 
+     * Export : value (boolean)                          *
+     * Purpose: Verify header is correct to knwon good   * 
+     *****************************************************/
+    public static boolean verifyHeader(String[] testHeader)
+    {
+        return csvHeader.equals(testHeader);
+    }
+
+    /*****************************************************
+     * Name   : csvReader                                *
+     * Date   : 08/10/2024                               *
+     * Import : None                                     * 
+     * Export : data (String[][])                        *
+     * Purpose: To read in the contents of a csv file    * 
+     *          and return them as a 2d string array     * 
+     *****************************************************/
+    public static String[][] csvReader()
+    {
+        File file = null;
+        FileInputStream fs = null;
+        InputStreamReader isr;
+        BufferedReader br;
+        String line;
+        boolean empty = true;
+        String[][] data = new String[][] {{""}};
+        
+        try
+        {
+            file = new File(filePath);
+            fs = new FileInputStream(file);
+            isr = new InputStreamReader(fs);
+            br = new BufferedReader(isr);
+            line = br.readLine();
+            while(line != null)
+            {
+                filePath = file.getAbsolutePath();
+                if (empty)
+                {
+                    data[0] = line.split(",");
+                    empty = false;
+                } else
+                {
+                    String[][] data1 = Arrays.copyOf(data, data.length + 1);
+                    data = data1;
+                    data[data.length-1] = line.split(",");
+                }
+                line = br.readLine();
+            }
+            fs.close();
+        }
+        catch (IOException e)
+        {
+            if (fs != null)
+            {
+                try
+                {
+                    fs.close();
+                }
+                catch (IOException e2)
+                {
+                    System.err.println("Error while trying to close file Stream: "+ e2.getMessage());
+                }
+            }
+            System.err.println("Error in file reading: " + e.getMessage());
+        }
+        return data;
+    }
+
+    /*****************************************************
+     * Name   : writeCSVLine                             *
+     * Date   : 08/10/2024                               *
+     * Import : data (String[]), file (FileOutputStream) *
+     * Export : None                                     *
+     * Purpose: To take an array of strings and write it *
+     *          to a line of a CSV file                  *
+     *****************************************************/
+    private static void writeCSVLine(String[] data, FileOutputStream file) throws IOException
+    {
+        for (int i = 0; i < (data.length - 1); i++)
+        {
+            file.write(data[i].getBytes());
+            file.write(",".getBytes());
+        }
+        file.write(data[data.length-1].getBytes());
+        file.write("\n".getBytes());
+    }
+
+    /*****************************************************
+     * Name   : writeCSV                                 *
+     * Date   : 08/10/2024                               *
+     * Import : None                                     *
+     * Export : None                                     *
+     * Purpose: To write out the library data to the CSV *
+     *          data file                                *
+     *****************************************************/
+    public static void writeCSV(Book[] books) 
+    {
+        FileOutputStream fileStream = null;
+        File tempFile = null;
+        File outFile = null;
+        try 
+        {
+            tempFile = new File(".libraryDataFile.tmp");
+            outFile = new File(filePath);
+            fileStream = new FileOutputStream(tempFile);
+            if (tempFile.exists())
+            {
+                writeCSVLine(csvHeader, fileStream);
+                
+                for (Book book : books)
+                {
+                    String[] array = new String[csvHeader.length];
+                    array[0] = book.getTitle();
+                    
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (i < book.getAuthorCount())
+                        {
+                            array[1+(i*4)] = book.getAuthors()[i].getLastName();
+                            array[2+(i*4)] = book.getAuthors()[i].getFirstName();
+                            array[3+(i*4)] = book.getAuthors()[i].getNationality();
+                            array[4+(i*4)] = book.getAuthors()[i].getBirthYear();
+                        }
+                        else
+                        {
+                            array[1+(i*4)] = "";
+                            array[2+(i*4)] = "";
+                            array[3+(i*4)] = "";
+                            array[4+(i*4)] = "";
+                        }
+                    }
+                    array[13] = book.getYear();
+                    array[14] = book.getISBN();
+                    if (book.isEbook())
+                    {
+                        array[15] = "TRUE";
+                    }
+                    else
+                    {
+                        array[15] = "FALSE";
+                    }
+                    array[16] = String.valueOf(book.getEdition());
+
+                    writeCSVLine(array, fileStream);
+                }
+                fileStream.flush();
+                fileStream.close();
+
+                tempFile.renameTo(outFile);
+            }
+            else
+            {
+                fileStream.flush();
+                fileStream.close();
+                throw new IOException("Temp file could not be created at: " + tempFile.getAbsolutePath());
+            }
+
+        }
+        catch (IOException e)
+        {
+            if (fileStream != null)
+            {
+                try
+                {
+                    fileStream.close();
+                }
+                catch (IOException e2)
+                {
+                    System.err.println("Error while trying to close file Stream: "+ e2.getMessage());
+                }
+            }
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Error: Could not write data file.");
+        }
+    }
+
+}
